71f4fa831502ddeb42b0702af5e03cbec62fedfb
Edgar Ramos <edgar_nathan@hotmail.com>
2017-07-21 12:01:08 -0600

    Primer commit

---
+++ resources/views/iniciar_seccion.blade.php
@@ -0,0 +1,44 @@
+@extends('master')
+
+@section('contenido')
+
+        <div class="login">
+          	<div class="wrap">
+				<div class="col_1_of_login span_1_of_login">
+					<h4 class="title">NUEVOS CLIENTES</h4>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan</p>
+					<div class="button1">
+					   <a href="register.html"><input type="submit" name="Submit" value="Regístrate aquí"></a>
+					 </div>
+					 <div class="clear"></div>
+				</div>
+				<div class="col_1_of_login span_1_of_login">
+				<div class="login-title">
+	           		<h4 class="title">CLIENTES REGISTRADOS</h4>
+					<div id="loginbox" class="loginbox">
+						<form action="" method="post" name="login" id="login-form">
+						  <fieldset class="input">
+						    <p id="login-form-username">
+						      <label for="modlgn_username">Correo Electrónico</label>
+						      <input id="modlgn_username" type="text" name="email" class="inputbox" size="18" autocomplete="off">
+						    </p>
+						    <p id="login-form-password">
+						      <label for="modlgn_passwd">Contraseña</label>
+						      <input id="modlgn_passwd" type="password" name="password" class="inputbox" size="18" autocomplete="off">
+						    </p>
+
+						    <div class="remember">
+							    <p id="login-form-remember">
+							      <label for="modlgn_remember"><a href="#">¿Olvidaste tu contraseña?</a></label>
+							   </p>
+							    <input type="submit" name="Submit" class="button" value="ENTRAR"><div class="clear"></div>
+							 </div>
+						  </fieldset>
+						 </form>
+					</div>
+			    </div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+ @stop
---
+++ resources/views/master.blade.php
@@ -0,0 +1,289 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="icon" href="images/logoico.ico">
+<title>Lunnetes</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/form.css" rel="stylesheet" type="text/css" media="all" />
+<link href='http://fonts.googleapis.com/css?family=Exo+2' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js/jquery1.min.js"></script>
+<!-- start menu -->
+<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<!--start slider -->
+    <link rel="stylesheet" href="css/fwslider.css" media="all">
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/css3-mediaqueries.js"></script>
+    <script src="js/fwslider.js"></script>
+<!--end slider -->
+<script src="js/jquery.easydropdown.js"></script>
+</head>
+<body>
+
+     <div class="header-top">
+	   <div class="wrap"> 
+			  <div class="cssmenu">
+				<ul>
+					<li><a href="checkout.html">Lista de deseos</a></li> |
+					<li><a href="login.html">Iniciar sesión</a></li> |
+					<li><a href="register.html">Regístrate aquí</a></li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+ 		</div>
+	</div>
+	<div class="header-bottom">
+	    <div class="wrap">
+			<div class="header-bottom-left">
+				<div class="logo">
+
+					<a href="index.html"><img src="images/logo1.png" height="50" width="160" alt=""/></a>
+
+				</div>
+				<div class="menu">
+	            <ul class="megamenu skyblue">
+					<li class="active grid"><a href="index.html">Inicio</a></li>
+					<li><a class="color4" href="womens.html">Mujeres</a></li>				
+					<li><a class="color5" href="mens.html">Hombres</a></li>
+				</ul>
+			</div>
+		</div>
+	   <div class="header-bottom-right">
+         <div class="search">	  
+				<input type="text" name="s" class="textbox" value="Buscar" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Buscar';}">
+				<input type="submit" value="Subscribe" id="submit" name="submit">
+				<div id="response"> </div>
+		 </div>
+	  <div class="tag-list">
+	    <ul class="icon1 sub-icon1 profile_img">
+			<li><a class="active-icon c1" href="#"> </a>
+				<ul class="sub-icon1 list">
+					<li><h3>sed diam nonummy</h3><a href=""></a></li>
+					<li><p>Lorem ipsum dolor sit amet, consectetuer  <a href="">adipiscing elit, sed diam</a></p></li>
+				</ul>
+			</li>
+		</ul>
+		<ul class="icon1 sub-icon1 profile_img">
+			<li><a class="active-icon c2" href="checkout.html"></a></li>
+		</ul>
+	    
+	  </div>
+    </div>
+     <div class="clear"></div>
+     </div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+			@yield('contenido')
+			</div>
+		</div>
+	</div>
+
+   <div class="footer">
+		<div class="footer-top">
+			<div class="wrap">
+			  <div class="section group example">
+				<div class="col_1_of_2 span_1_of_2">
+					<ul class="f-list">
+					  <li><img src="images/2.png"><span class="f-text">ENVÍO GRATUITO EN PEDIDOS SUPERIORES A $100</span><div class="clear"></div></li>
+					</ul>
+				</div>
+				<div class="col_1_of_2 span_1_of_2">
+					<ul class="f-list">
+					  <li><img src="images/3.png"><span class="f-text">LLÁMENOS! SIN CARGO-222-555-6666</span><div class="clear"></div></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
+		      </div>
+			</div>
+		</div>
+		<div class="footer-middle">
+			<div class="wrap">
+			 <!-- <div class="section group">
+			  	<div class="f_10">
+					<div class="col_1_of_4 span_1_of_4">
+						<h3>Facebook</h3>
+						<script>(function(d, s, id) {
+						  var js, fjs = d.getElementsByTagName(s)[0];
+						  if (d.getElementById(id)) return;
+						  js = d.createElement(s); js.id = id;
+						  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+						  fjs.parentNode.insertBefore(js, fjs);
+						}(document, 'script', 'facebook-jssdk'));</script>
+						<div class="like_box">	
+							<div class="fb-like-box" data-href="http://www.facebook.com/w3layouts" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+						</div>
+					</div>
+					<div class="col_1_of_4 span_1_of_4">
+						<h3>From Twitter</h3>
+						<div class="recent-tweet">
+							<div class="recent-tweet-icon">
+								<span> </span>
+							</div>
+							<div class="recent-tweet-info">
+								<p>Ds which don't look even slightly believable. If you are <a href="#">going to use nibh euismod</a> tincidunt ut laoreet adipisicing</p>
+							</div>
+							<div class="clear"> </div>
+						</div>
+						<div class="recent-tweet">
+							<div class="recent-tweet-icon">
+								<span> </span>
+							</div>
+							<div class="recent-tweet-info">
+								<p>Ds which don't look even slightly believable. If you are <a href="#">going to use nibh euismod</a> tincidunt ut laoreet adipisicing</p>
+							</div>
+							<div class="clear"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="f_10">
+					<div class="col_1_of_4 span_1_of_4">
+					    <h3>Information</h3>
+						<ul class="f-list1">
+						    <li><a href="#">Duis autem vel eum iriure </a></li>
+				            <li><a href="#">anteposuerit litterarum formas </a></li>
+				            <li><a href="#">Tduis dolore te feugait nulla</a></li>
+				             <li><a href="#">Duis autem vel eum iriure </a></li>
+				            <li><a href="#">anteposuerit litterarum formas </a></li>
+				            <li><a href="#">Tduis dolore te feugait nulla</a></li>
+			         	</ul>
+					</div>
+					<div class="col_1_of_4 span_1_of_4">
+						<h3>Contact us</h3>
+						<div class="company_address">
+					                <p>500 Lorem Ipsum Dolor Sit,</p>
+							   		<p>22-56-2-9 Sit Amet, Lorem,</p>
+							   		<p>USA</p>
+					   		<p>Phone:(00) 222 666 444</p>
+					   		<p>Fax: (000) 000 00 00 0</p>
+					 	 	<p>Email: <span>mail[at]leoshop.com</span></p>
+					   		
+					   </div>
+					   <div class="social-media">
+						     <ul>
+						        <li> <span class="simptip-position-bottom simptip-movable" data-tooltip="Google"><a href="#" target="_blank"> </a></span></li>
+						        <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Linked in"><a href="#" target="_blank"> </a> </span></li>
+						        <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Rss"><a href="#" target="_blank"> </a></span></li>
+						        <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Facebook"><a href="#" target="_blank"> </a></span></li>
+						    </ul>
+					   </div>
+					</div>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
+		  </div>-->
+		   
+		   
+		   
+		   
+		   
+		   <div class="section group example">
+			  <div class="col_1_of_f_1 span_1_of_f_1">
+				 <div class="section group example">
+				   <div class="col_1_of_f_2 span_1_of_f_2">
+				      <h3>Facebook</h3>
+						<script>(function(d, s, id) {
+						  var js, fjs = d.getElementsByTagName(s)[0];
+						  if (d.getElementById(id)) return;
+						  js = d.createElement(s); js.id = id;
+						  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+						  fjs.parentNode.insertBefore(js, fjs);
+						}(document, 'script', 'facebook-jssdk'));</script>
+						<div class="like_box">	
+							<div class="fb-like-box" data-href="http://www.facebook.com/w3layouts" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+						</div>
+ 				  </div>
+				  <div class="col_1_of_f_2 span_1_of_f_2">
+						<h3>DESDE TWITTER</h3>
+				       <div class="recent-tweet">
+							<div class="recent-tweet-icon">
+								<span> </span>
+							</div>
+							<div class="recent-tweet-info">
+								<p>Ds which don't look even slightly believable. If you are <a href="#">going to use nibh euismod</a> tincidunt ut laoreet adipisicing</p>
+							</div>
+							<div class="clear"> </div>
+					   </div>
+					   <div class="recent-tweet">
+							<div class="recent-tweet-icon">
+								<span> </span>
+							</div>
+							<div class="recent-tweet-info">
+								<p>Ds which don't look even slightly believable. If you are <a href="#">going to use nibh euismod</a> tincidunt ut laoreet adipisicing</p>
+							</div>
+							<div class="clear"> </div>
+					  </div>
+				</div>
+				<div class="clear"></div>
+		      </div>
+ 			 </div>
+			 <div class="col_1_of_f_1 span_1_of_f_1">
+			   <div class="section group example">
+				 <div class="col_1_of_f_2 span_1_of_f_2">
+				    <h3>INFORMACIÓN</h3>
+						<ul class="f-list1">
+						    <li><a href="#">Duis autem vel eum iriure </a></li>
+				            <li><a href="#">anteposuerit litterarum formas </a></li>
+				            <li><a href="#">Tduis dolore te feugait nulla</a></li>
+				             <li><a href="#">Duis autem vel eum iriure </a></li>
+				            <li><a href="#">anteposuerit litterarum formas </a></li>
+				            <li><a href="#">Tduis dolore te feugait nulla</a></li>
+			         	</ul>
+ 				 </div>
+				 <div class="col_1_of_f_2 span_1_of_f_2">
+				   <h3>CONTÁCTENOS</h3>
+						<div class="company_address">
+					                <p>500 Lorem Ipsum Dolor Sit,</p>
+							   		<p>22-56-2-9 Sit Amet, Lorem,</p>
+							   		<p>USA</p>
+					   		<p>Phone:(00) 222 666 444</p>
+					   		<p>Fax: (000) 000 00 00 0</p>
+					 	 	<p>Email: <span>mail[at]leoshop.com</span></p>
+					   		
+					   </div>
+					   <div class="social-media">
+						     <ul>
+						        <li> <span class="simptip-position-bottom simptip-movable" data-tooltip="Google"><a href="#" target="_blank"> </a></span></li>
+						        <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Linked in"><a href="#" target="_blank"> </a> </span></li>
+						        <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Rss"><a href="#" target="_blank"> </a></span></li>
+						        <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Facebook"><a href="#" target="_blank"> </a></span></li>
+						    </ul>
+					   </div>
+				</div>
+				<div class="clear"></div>
+		    </div>
+		   </div>
+		  <div class="clear"></div>
+		    </div>
+		  </div>
+		</div>
+		<div class="footer-bottom">
+			<div class="wrap">
+	             <div class="copy">
+			        <p>Lunnettes©2017<a href="http://w3layouts.com" target="_blank"></a></p>
+		         </div>
+				<div class="f-list2">
+				 <ul>
+					<li class="active"><a href="about.html">Sobre Nosotros</a></li> |
+					<li><a href="delivery.html">Entregas y Devoluciones</a></li> |
+					<li><a href="delivery.html">Términos y Condiciones</a></li> |
+					<li><a href="contact.html">Contáctenos</a></li> 
+				 </ul>
+			    </div>
+			    <div class="clear"></div>
+		      </div>
+	     </div>
+	</div>
+</body>
+</html>
