2ff83a783795f3846511967405089103400a16b5
Selena Alanis <selena23_alanisV@hotmail.com>
2017-07-22 14:38:52 -0600

    Se le agrego las imagenes y la parte de las categorias

---
+++ resources/views/gafasMujeres.blade.php
@@ -0,0 +1,476 @@
+@extends('master')
+@section('contenido')
+<div class="mens">    
+  <div class="main">
+     <div class="wrap">
+		<div class="cont span_2_of_3">
+		  	<h2 class="head">Mujeres</h2>
+		  	<div class="mens-toolbar">
+              <div class="sort">
+               	<div class="sort-by">
+		            <label>Ordenar por :</label>
+		            <select>
+		                            <option value="">
+		                    Posición                </option>
+		                            <option value="">
+		                    Nombre                </option>
+		                            <option value="">
+		                    Precio                </option>
+		            </select>
+		            <a href=""><img src="images/arrow2.gif" alt="" class="v-middle"></a>
+               </div>
+    		</div>
+        <div class="pager">   
+        	<div class="limiter visible-desktop">
+            <label>Espectáculo</label>
+            <select>
+                            <option value="" selected="selected">
+                    9                </option>
+                            <option value="">
+                    15                </option>
+                            <option value="">
+                    30                </option>
+                        </select> por página        
+             </div>
+       		<ul class="dc_pagination dc_paginationA dc_paginationA06">
+			    <li><a href="#" class="previous">Páginas</a></li>
+			    <li><a href="#">1</a></li>
+			    <li><a href="#">2</a></li>
+		  	</ul>
+	   		<div class="clear"></div>
+    	</div>
+     	<div class="clear"></div>
+	  </div>
+			<div class="top-box">
+			 <div class="col_1_of_3 span_1_of_3"> 
+			   <a href="single.html">
+				<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic.jpg" alt=""/>
+					</div>
+                    <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                 </a>
+				</div>
+			   <div class="col_1_of_3 span_1_of_3">
+			   	 <a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic1.jpg" alt=""/>
+					</div>
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+				  <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic2.jpg" alt=""/>
+					</div>
+                    <div class="sale-box1"><span class="on_sale title_shop">Oferta</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="reducedfrom">$66.00</span>
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="clear"></div>
+			</div>	
+			<div class="top-box">
+			  <div class="col_1_of_3 span_1_of_3">
+			  	 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic3.jpg" alt=""/>
+					</div>
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+					<a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic4.jpg" alt=""/>
+					</div>
+					 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic5.jpg" alt=""/>
+					</div>
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                 </a>
+				</div>
+				<div class="clear"></div>
+			</div>	
+			<div class="top-box1">
+			  <div class="col_1_of_3 span_1_of_3">
+			  	 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic6.jpg" alt=""/>
+					</div>
+                     <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic7.jpg" alt=""/>
+					</div>
+					 <div class="sale-box1"><span class="on_sale title_shop">Oferta</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="reducedfrom">$66.00</span>
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				  <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic8.jpg" alt=""/>
+					</div>
+                   	 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="clear"></div>
+			</div>	
+		 <h2 class="head">Sección personal</h2>
+		  <div class="top-box1">
+			  <div class="col_1_of_3 span_1_of_3">
+			  	 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic8.jpg" alt=""/>
+					</div>
+                     <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+					 <a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic4.jpg" alt=""/>
+					</div>
+				    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic2.jpg" alt=""/>
+					</div>
+                   	 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="clear"></div>
+			</div>	
+		   <h2 class="head">Nuevos productos</h2>	
+		    <div class="section group">
+			  <div class="col_1_of_3 span_1_of_3">
+			  	 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic5.jpg" alt=""/>
+					</div>
+                     <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+					<a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic2.jpg" alt=""/>
+					</div>
+					 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic3.jpg" alt=""/>
+					</div>
+                   	 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="clear"></div>
+			</div>			 							 			    
+		  </div>
+			<div class="rsidebar span_1_of_left">
+				<h5 class="m_1">Categorias</h5>
+					<select class="dropdown" tabindex="8" data-settings='{"wrapperClass":"metro"}'>
+						<option value="1">Hombres</option>
+						<option value="2">Sub Category1</option>
+						<option value="3">Sub Category2</option>
+						<option value="4">Sub Category3</option>
+					</select>
+					<select class="dropdown" tabindex="8" data-settings='{"wrapperClass":"metro"}'>
+						<option value="1">Mujeres</option>
+						<option value="2">Sub Category1</option>
+						<option value="3">Sub Category2</option>
+						<option value="4">Sub Category3</option>
+					</select>
+					<ul class="kids">
+						<li><a href="#">Niños</a></li>
+						<li class="last"><a href="#">Tienda de gafas</a></li>
+					</ul>
+                   <section  class="sky-form">
+					<h4>Precio</h4>
+						<div class="row row1 scroll-pane">
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i></i>Rs 500 - Rs 700</label>
+							</div>
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 700 - Rs 1000</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 1000 - Rs 1500</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 1500 - Rs 2000</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 2000 - Rs 2500</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox" ><i></i>Rs 2500 - Rs 3000</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 3500 - Rs 4000</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 4000 - Rs 4500</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 4500 - Rs 5000</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 5000 - Rs 5500</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 5500 - Rs 6000</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 6000 - Rs 6500</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 6500 - Rs 7000</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 7000 - Rs 7500</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 7500 - Rs 8000</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rs 8000 - Rs 8500</label>	
+							</div>
+						</div>
+		        </section>
+		       <section  class="sky-form">
+					<h4>Nombre de la marca</h4>
+						<div class="row row1 scroll-pane">
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i></i>Gucci</label>
+							</div>
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Armani</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Prada</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Silhouette</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Polo Ralph Lauren</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox" ><i></i>Fendi</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Guess</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Nike</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Vogue</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Kate Spade</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Ray Ban</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Clavin Klein</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Police</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Tomy Hilfiger</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Tom Ford</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Animal</label>	
+							</div>
+						</div>
+		       </section>
+		       <section  class="sky-form">
+					<h4>Forma de la mondura</h4>
+						<div class="row row1 scroll-pane">
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i></i>Pilot</label>
+							</div>
+							<div class="col col-4">
+								
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rectangular</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Cuadrado</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Redondo</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox" ><i></i>Otros</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Ojos de Gato</label>
+								
+							</div>
+						</div>
+		       </section>
+		       <section  class="sky-form">
+					<h4>Tamaño del marco</h4>
+						<div class="row row1 scroll-pane">
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i></i>Pequeño</label>
+							</div>
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Mediano</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Grande</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Mediano</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Grande</label>
+							</div>
+						</div>
+		       </section>
+		       <section  class="sky-form">
+					<h4>Tipo de marco</h4>
+						<div class="row row1 scroll-pane">
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i></i>Full Rim</label>
+							</div>
+							<div class="col col-4">
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rim Less</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Half Rim</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Rim Less</label>
+								<label class="checkbox"><input type="checkbox" name="checkbox"><i></i>Half Rim</label>
+							</div>
+						</div>
+		       </section>
+		       <section  class="sky-form">
+					<h4>Colores</h4>
+						<ul class="color-list">
+							<li><a href="#"> <span class="color1"> </span><p class="red">Rojo</p></a></li>
+							<li><a href="#"> <span class="color2"> </span><p class="red">Verde</p></a></li>
+							<li><a href="#"> <span class="color3"> </span><p class="red">Azul</p></a></li>
+							<li><a href="#"> <span class="color4"> </span><p class="red">Amarillo</p></a></li>
+							<li><a href="#"> <span class="color5"> </span><p class="red">Violeta</p></a></li>
+							<li><a href="#"> <span class="color6"> </span><p class="red">Naranja</p></a></li>
+							<li><a href="#"> <span class="color7"> </span><p class="red">Gris</p></a></li>
+					   </ul>
+		       </section>
+		      </div>
+			<div class="clear"></div>
+			</div>
+		   </div>
+		</div>
+		<script src="js/jquery.easydropdown.js"></script>
+@stop
