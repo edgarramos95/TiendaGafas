927d14b2f1e923d0c045dc4ffc57af26f090404b
Selena Alanis <selena23_alanisV@hotmail.com>
2017-07-22 14:49:45 -0600

    Se agrego lo de la bolsa de compra

---
+++ resources/views/revisarCompras.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+@section('contenido')
+         <div class="register_account">
+           <div class="wrap">
+    	     <h4 class="title">La bolsa de compra esta vacía</h4>
+    	     <p class="cart">No contiene artículos el carrito de compras.<br>Click<a href="{{url('/')}}"> aquí</a> para seguir comprando</p>
+    	   </div>
+    	</div>
+ @stop
