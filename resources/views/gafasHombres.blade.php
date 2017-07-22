dcb772a64f048a5e44fcf07cf2c00edb73c3bc0c
Selena Alanis <selena23_alanisV@hotmail.com>
2017-07-22 14:03:45 -0600

    Se agrego imagenes en la vista

---
+++ resources/views/gafasHombres.blade.php
@@ -0,0 +1,345 @@
+@extends('master')
+@section('contenido')
+<div class="mens">    
+  <div class="main">
+     <div class="wrap">
+		<div class="cont span_2_of_3">
+		  	<h2 class="head">Hombres</h2>
+		  	<div class="mens-toolbar">
+              <div class="sort">
+               	<div class="sort-by">
+		            <label>Ordenar por :</label>
+		            <select>
+		                            <option value="">
+		                    Posición                </option>
+		                            <option value="">
+		                    Nombre                </option>
+		                            <option value="">
+		                    Precio                </option>
+		            </select>
+		            <a href=""><img src="images/arrow2.gif" alt="" class="v-middle"></a>
+               </div>
+    		</div>
+        <div class="pager">   
+        	<div class="limiter visible-desktop">
+            <label>Espectáculo</label>
+            <select>
+                            <option value="" selected="selected">
+                    9                </option>
+                            <option value="">
+                    15                </option>
+                            <option value="">
+                    30                </option>
+                        </select> por página        
+             </div>
+       		<ul class="dc_pagination dc_paginationA dc_paginationA06">
+			    <li><a href="#" class="previous">Páginas</a></li>
+			    <li><a href="#">1</a></li>
+			    <li><a href="#">2</a></li>
+		  	</ul>
+	   		<div class="clear"></div>
+    	</div>
+     	<div class="clear"></div>
+	</div>
+			<div class="top-box">
+			 <div class="col_1_of_3 span_1_of_3"> 
+			   <a href="single.html">
+				<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic.jpg" alt=""/>
+					</div>
+                    <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                 </a>
+				</div>
+			   <div class="col_1_of_3 span_1_of_3">
+			   	 <a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic1.jpg" alt=""/>
+					</div>
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+				  <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic2.jpg" alt=""/>
+					</div>
+                    <div class="sale-box1"><span class="on_sale title_shop">Oferta</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="reducedfrom">$66.00</span>
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="clear"></div>
+			</div>	
+			<div class="top-box">
+			  <div class="col_1_of_3 span_1_of_3">
+			  	 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic3.jpg" alt=""/>
+					</div>
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+					<a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic4.jpg" alt=""/>
+					</div>
+					 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic5.jpg" alt=""/>
+					</div>
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                 </a>
+				</div>
+				<div class="clear"></div>
+			</div>	
+			<div class="top-box1">
+			  <div class="col_1_of_3 span_1_of_3">
+			  	 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic6.jpg" alt=""/>
+					</div>
+                     <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic7.jpg" alt=""/>
+					</div>
+					 <div class="sale-box1"><span class="on_sale title_shop">Oferta</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="reducedfrom">$66.00</span>
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				  <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic8.jpg" alt=""/>
+					</div>
+                   	 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="clear"></div>
+			</div>	
+		 <h2 class="head">Sección personal</h2>
+		  <div class="top-box1">
+			  <div class="col_1_of_3 span_1_of_3">
+			  	 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic8.jpg" alt=""/>
+					</div>
+                     <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+					 <a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic4.jpg" alt=""/>
+					</div>
+				    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic2.jpg" alt=""/>
+					</div>
+                   	 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="clear"></div>
+			</div>	
+		   <h2 class="head">Nuevos productos</h2>	
+		    <div class="section group">
+			  <div class="col_1_of_3 span_1_of_3">
+			  	 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic5.jpg" alt=""/>
+					</div>
+                     <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+					<a href="single.html">
+					<div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic2.jpg" alt=""/>
+					</div>
+					 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="col_1_of_3 span_1_of_3">
+				 <a href="single.html">
+				 <div class="inner_content clearfix">
+					<div class="product_image">
+						<img src="images/pic3.jpg" alt=""/>
+					</div>
+                   	 <div class="sale-box"><span class="on_sale title_shop">Nuevo</span></div>	
+                    <div class="price">
+					   <div class="cart-left">
+							<div class="price1">
+							  <span class="actual">$12.00</span>
+							</div>
+						</div>
+						<div class="cart-right"> </div>
+						<div class="clear"></div>
+					 </div>				
+                   </div>
+                   </a>
+				</div>
+				<div class="clear"></div>
+			</div>			 							 			    
+		  </div>
+@stop
