ac84e0cfe7c2405fb7bad0dd5625e303cb77ba9a
Selena Alanis <selena23_alanisV@hotmail.com>
2017-07-22 12:31:49 -0600

    Testimonios en la vista mision y vision

---
+++ resources/views/misionVision.blade.php
@@ -0,0 +1,28 @@
+@extends('master')
+@section('contenido')
+  	   <div class="login">
+          <div class="wrap">
+				<ul class="breadcrumb breadcrumb__t"><a class="home" href="#">Inicio</a>  / Cuenta</ul>
+				<div class="section group">
+				   <div class="labout span_1_of_about">
+					 <h3>Testimonios</h3>
+					  <div class="testimonials ">
+						<div class="testi-item">
+						<blockquote class="testi-item_blockquote">
+							<a href="#">"Hemos conseguido los lentes que había necesitado, me encanto el diseño que se utilizo" </a>
+							<div class="clear"></div>
+						</blockquote>
+							<small class="testi-meta"><span class="user">zahory_reyes@hotmail.com</span>
+							<span class="info"></span><br><a href="#">http://demolink.org</a></small></div>
+					   </div>
+					   <div class="testimonials ">
+						<div class="testi-item">
+						<blockquote class="testi-item_blockquote">
+							<a href="#">"Sin duda alguna la mejor tienda en línea de lentes, nunca tuve algún problema, me llegaron a tiempo". </a>
+							<div class="clear"></div>
+						</blockquote>
+							<small class="testi-meta"><span class="user">cristina_chavez@hotmail.com</span>
+							<span class="info"></span><br><a href="#">http://demolink.org</a></small></div>
+					   </div>
+				    </div>
+@stop
