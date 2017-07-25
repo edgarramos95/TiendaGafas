6dfbbdf20b2ebc528de2674451607ce271418909
Edgar Ramos <edgar_nathan@hotmail.com>
2017-07-25 00:08:23 -0600

    Cambio de nombre de registro por registroCliente

--- app/Http/Controllers/proyectoController.php
+++ app/Http/Controllers/proyectoController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Cliente;
+
 use App\Http\Controllers\Controller;
 
 class proyectoController extends Controller
 {
     public function registrarCli()
     {
-    	
+    	$clientes=Cliente::all();
+
+    	return view('registrarCliente');
     }
 }
